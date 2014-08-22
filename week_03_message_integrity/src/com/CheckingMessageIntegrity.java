56f7e9a0816d29fe251f2917142fcc74dfa03397
AntonioDiaz <toni@toni-HP>
2014-08-22 19:19:15 +0200

    algorithm 03

--- week_03_message_integrity/src/com/CheckingMessageIntegrity.java
+++ week_03_message_integrity/src/com/CheckingMessageIntegrity.java
@@ -22,14 +22,6 @@ public class CheckingMessageIntegrity {
 	public static final String OUTPUT = "C:/Users/toni/git/cryptography_coursera03/week_03_message_integrity/output/output.txt";
 	public static final int BLOCK_SIZE = 1024;
 
-	public static String bytesToHexadecimal(byte[] myBytes) {
-		StringBuilder cadena = new StringBuilder();
-		for (byte b : myBytes) {
-			cadena.append(String.format("%02X", b));
-		}
-		return cadena.toString();
-	}
-	
 	/**
 	 * 
 	 * @param args
@@ -38,41 +30,22 @@ public class CheckingMessageIntegrity {
 	 * @throws CloneNotSupportedException
 	 */
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, CloneNotSupportedException {
-		DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-		String output = OUTPUT.replaceAll(".txt", dateFormat.format(new Date()) + ".txt");
-		PrintWriter printWriter = new PrintWriter(output);
 		File file = new File(EXERCISE_VIDEO);
-		System.out.println(file.length());
-		printWriter.println(file.length());
 		int blocksNumber = (int)file.length() / BLOCK_SIZE;
-		System.out.println("Bloques: " + blocksNumber + "\n");
-		printWriter.println("Bloques: " + blocksNumber + "\n");
-		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 		byte[] previousHsa = new byte[0];
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 		for (int i = blocksNumber; i >=0; i--) {
+			messageDigest.reset();
 			long begin = i * BLOCK_SIZE;
 			int size = BLOCK_SIZE;
 			if (begin + size > file.length()) {
 				size = (int)file.length() - (int)begin;
 			}
-			System.out.println("i: " + i);
-			printWriter.println("i: " + i);
 			byte[] readFromFile = readFromFile(TRAINING_VIDEO, begin, size);
-			System.out.println(readFromFile.length + "-" + bytesToHexadecimal(readFromFile));
-			printWriter.println(readFromFile.length + "-" + bytesToHexadecimal(readFromFile));
 			readFromFile = ArrayUtils.addAll(readFromFile, previousHsa);
 			messageDigest.update(readFromFile);
-			System.out.println(readFromFile.length + "-" + bytesToHexadecimal(readFromFile));
-			printWriter.println(readFromFile.length + "-" + bytesToHexadecimal(readFromFile));
 			previousHsa = messageDigest.digest();
-			System.out.println(previousHsa.length + "-" + bytesToHexadecimal(previousHsa));
-			printWriter.println(previousHsa.length + "-" + bytesToHexadecimal(previousHsa));
-			System.out.println("----");
-			printWriter.println("----");
 		}
-		System.out.println("Resultado: " + bytesToHexadecimal(previousHsa));		
-		printWriter.println("Resultado: " + bytesToHexadecimal(previousHsa));
-		printWriter.close();
 	}
 
 	private static byte[] readFromFile(String filePath, long position, int size) throws IOException {
@@ -83,4 +56,13 @@ public class CheckingMessageIntegrity {
 		file.close();
 		return bytes;
 	}
+	
+	public static String bytesToHexadecimal(byte[] myBytes) {
+		StringBuilder cadena = new StringBuilder();
+		for (byte b : myBytes) {
+			cadena.append(String.format("%02X", b));
+		}
+		return cadena.toString();
+	}
+		
 }
